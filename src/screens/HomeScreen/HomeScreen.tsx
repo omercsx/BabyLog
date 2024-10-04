508659b02dc669b7588e317f8476e2e9da939549
omercsx <omercagris@gmail.com>
2024-10-04 07:04:22 -0400

    welcome text in home screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {StyleSheet, Text, View} from 'react-native';
+import colors from '../../constants/colors';
+import Welcome from './Welcome';
+
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Welcome />
+      <Text style={styles.logActivitiesText}>Log activities</Text>
     </View>
-  )
-}
-export default Home
-const styles = StyleSheet.create({})
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+
+  logActivitiesText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginHorizontal: 20,
+  },
+
+  upperContainer: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+});
---
+++ src/screens/HomeScreen/Welcome.tsx
@@ -0,0 +1,38 @@
+import {useEffect, useState} from 'react';
+import {getCurrentUser, type AuthUser} from 'aws-amplify/auth';
+import {StyleSheet, Text} from 'react-native';
+
+const Welcome = () => {
+  const [user, setUser] = useState<AuthUser | undefined>(undefined);
+
+  useEffect(() => {
+    getCurrentUser().then(user => {
+      setUser(user);
+    });
+  }, []);
+
+  const time = new Date().getHours();
+  let greeting = '';
+
+  if (time < 12) {
+    greeting = 'Good morning';
+  } else if (time < 18) {
+    greeting = 'Good afternoon';
+  } else {
+    greeting = 'Good evening';
+  }
+  return (
+    <Text style={styles.welcomeText}>
+      {greeting}, {user?.username ? user.username : 'Omer'}
+    </Text>
+  );
+};
+export default Welcome;
+const styles = StyleSheet.create({
+  welcomeText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+});
