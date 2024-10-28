ef61c7522d2338c18c6fea99996cde4bcbba8dd4
omercsx <omercagris@gmail.com>
2024-10-28 07:34:52 -0400

    Refactor bottom tab navigator to use custom icons

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,8 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import HomeStackNavigator from './HomeStackNavigator';
 import ProfileScreen from '../screens/ProfileScreen';
-
 import type { BottomTabNavigatorParamList } from '../types/navigation';
 import NewsFeedScreen from '../screens/NewsFeedScreen';
 
@@ -10,7 +10,24 @@ const Tab = createBottomTabNavigator<BottomTabNavigatorParamList>();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = 'help-outline'; // Add default icon
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'News Feed') {
+            iconName = focused ? 'newspaper' : 'newspaper-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#F5C754',
+        tabBarInactiveTintColor: 'gray',
+      })}>
       <Tab.Screen
         name="Home"
         component={HomeStackNavigator}
